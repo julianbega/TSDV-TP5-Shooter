11a20ba9dce7f352b7c898eb548387449e46a3ba
Julian Bega <julian.begamilrad@gmail.com>
2021-05-02 23:12:19 -0300

    Player

---
+++ TP5-Shooter-BegaJulian/Assets/Scripts/Gun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    enum GunsIDs {Pistol, BallLauncher};
+    public string Name;
+    public int GunID;
+    public int TotalBullets;
+    public int BulletsPerClip;
+    public int TimeToReload;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TP5-Shooter-BegaJulian/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+
+    public int PlayerHP;
+    public Gun[] ActualGun;
+    public int GunRange;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
