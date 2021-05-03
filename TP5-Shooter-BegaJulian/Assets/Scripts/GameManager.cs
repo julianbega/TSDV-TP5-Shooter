e1a90587457ddab0f07185d8f16103587b41dcb7
Julian Bega <julian.begamilrad@gmail.com>
2021-05-02 23:11:26 -0300

    GameManager

---
+++ TP5-Shooter-BegaJulian/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public int BombDamage;
+    public int TimeToSpawnBomb;
+    public int PointsForDestroyingBomb;
+
+    public int PointsPerBox;
+    public int TimeToSpawnBox;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
