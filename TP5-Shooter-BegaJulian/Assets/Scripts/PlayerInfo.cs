277c0457b5f388c4710dd0607f930adfe674ddf2
Julian Bega <julian.begamilrad@gmail.com>
2021-05-05 00:34:57 -0300

    ChangeGun

--- TP5-Shooter-BegaJulian/Assets/Scripts/PlayerInfo.cs
+++ TP5-Shooter-BegaJulian/Assets/Scripts/PlayerInfo.cs
@@ -11,7 +11,7 @@ public class PlayerInfo : MonoBehaviour
 
     public int PlayerHP;
     public Text HP;
-    public Gun[] ActualGun;
+    public int ActualGun;
 
     public GameManager gameManager;
 
@@ -33,6 +33,20 @@ public class PlayerInfo : MonoBehaviour
             changeUI?.Invoke(PlayerHP);
 
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActualGun = 0;
+            activatePistol();
+            deActivateBallThrower();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ActualGun = 1;
+            deActivatePistol();
+            activateBallThrower();
+
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -52,5 +66,38 @@ public class PlayerInfo : MonoBehaviour
         {
             PlayerHP -= 10;
         }
+    }
+
+    public void activateBallThrower()
+    {
+        GameObject.Find("BallThrower").gameObject.GetComponent<Gun>().enabled = true;
+        GameObject.Find("BallThrowerBigHandler").gameObject.GetComponent<MeshRenderer>().enabled = true;
+        GameObject.Find("BallThrowerCannon").gameObject.GetComponent<MeshRenderer>().enabled = true;
+        GameObject.Find("BallThrowerCharger").gameObject.GetComponent<MeshRenderer>().enabled = true;
+        GameObject.Find("BallThrowerSmallHandler").gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+    public void deActivateBallThrower()
+    {
+        GameObject.Find("BallThrower").gameObject.GetComponent<Gun>().enabled = false;
+        GameObject.Find("BallThrowerBigHandler").gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GameObject.Find("BallThrowerCannon").gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GameObject.Find("BallThrowerCharger").gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GameObject.Find("BallThrowerSmallHandler").gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+    public void activatePistol()
+    {
+        GameObject.Find("Pistol").gameObject.GetComponent<Gun>().enabled = true;
+        GameObject.Find("Pistol").gameObject.GetComponent<MeshRenderer>().enabled = true;
+        GameObject.Find("PistolHandler").gameObject.GetComponent<MeshRenderer>().enabled = true;
+        GameObject.Find("PistolTrigerHandler").gameObject.GetComponent<MeshRenderer>().enabled = true;
+        GameObject.Find("PistolTrigger").gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+    public void deActivatePistol()
+    {
+        GameObject.Find("Pistol").gameObject.GetComponent<Gun>().enabled = false;
+        GameObject.Find("Pistol").gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GameObject.Find("PistolHandler").gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GameObject.Find("PistolTrigerHandler").gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GameObject.Find("PistolTrigger").gameObject.GetComponent<MeshRenderer>().enabled = false;
     }
 }
