1bcbf85598777bb32099f2b448bb0035c06d63f4
Julian Bega <julian.begamilrad@gmail.com>
2021-05-05 00:49:11 -0300

    Ball for the ballThrower

---
+++ TP5-Shooter-BegaJulian/Assets/Scripts/TimeToDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeToDestroy : MonoBehaviour
+{
+    public float timeToDeleteItself;
+    void Update()
+    {
+        timeToDeleteItself -= Time.deltaTime;
+        if (timeToDeleteItself <= 0) Destroy(this.gameObject);
+    }
+}
